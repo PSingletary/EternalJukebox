ef3e5a5695ca8d3fbd1229c058b7d1157bc9f06a
Izzy <undermybrella@abimon.org>
2025-01-22 23:11:59 +1100

    fix(youtube): Fix the Windows batch file erroneously splitting YouTube URLs into two parameters

--- yt.bat
+++ yt.bat
@@ -1,1 +1,5 @@
+IF "%~4"=="" GOTO download
+SET %2="%1=%2"
+SHIFT
+:download
 yt-dlp --audio-format %3 -x -o %2 --max-filesize 100m --no-playlist --max-downloads 1 --playlist-end 1 --exec "echo Video ID: %%(id)s" -- "%1"
